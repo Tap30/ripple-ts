75ddfcecc91aed8756d8d287a46899b707feed76
Mostafa Shamsitabar <mostafa.sh.coderino@gmail.com>
2025-12-09 13:19:18 +0330

    chore(tests): update test config

--- packages/browser/vitest.config.ts
+++ packages/browser/vitest.config.ts
@@ -3,19 +3,17 @@ import { coverageConfigDefaults, defineConfig } from "vitest/config";
 const config = defineConfig({
   test: {
     environment: "jsdom",
-    include: [
-      "src/**/__tests__/**/*.[jt]s?(x)",
-      "src/**/?(*.)+(spec|test).[jt]s?(x)",
-    ],
+    include: ["src/**/?(*.)+test.[jt]s?(x)"],
     coverage: {
       enabled: true,
       provider: "v8",
-      reporter: ["text", "clover", "json-summary"],
-      include: ["src/**"],
+      reporter: ["text"],
+      include: ["src/**/*.ts?(x)"],
       exclude: [
         ...coverageConfigDefaults.exclude,
-        "src/index.ts",
+        "src/**/index.ts",
         "src/**/types.ts",
+        "src/**/coverage/**",
       ],
       thresholds: {
         "100": true,
